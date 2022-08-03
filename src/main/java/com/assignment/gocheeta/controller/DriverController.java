deecec8beff476b742e74d9f1d33a619550256c4
Kalana <macbookair@MacBooks-MacBook-Air.local>
2022-08-03 23:01:38 +0530

    driver get half done

--- src/main/java/com/assignment/gocheeta/controller/DriverController.java
+++ src/main/java/com/assignment/gocheeta/controller/DriverController.java
@@ -1,15 +1,20 @@
 package com.assignment.gocheeta.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 import com.assignment.gocheeta.model.Driver;
 import com.assignment.gocheeta.services.DriverService;
 
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/")
 public class DriverController {
 
@@ -22,6 +27,11 @@ public class DriverController {
     @PostMapping("/createDriver")
     public Driver createDriver(@RequestBody Driver driver){
         return driverService.createDriver(driver);
+    }
+
+    @GetMapping("/getDrivers")
+    public List<Driver> getAllDrivers(){
+        return driverService.getAllDrivers();
     }
     
 }
--- src/main/java/com/assignment/gocheeta/services/DriverService.java
+++ src/main/java/com/assignment/gocheeta/services/DriverService.java
@@ -2,8 +2,13 @@ package com.assignment.gocheeta.services;
 
 import com.assignment.gocheeta.model.Driver;
 
+import java.util.List;
+
+
 public interface DriverService {
 
     Driver createDriver(Driver driver);
     
+    List<Driver> getAllDrivers();
+
 }
